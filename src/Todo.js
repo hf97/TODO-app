292025d8138bd29834d76ddf6306ad4e20a3187f
HF97 <hugoanf97@gmail.com>
2020-08-31 16:51:06 +0100

    final deploy

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,52 @@
-import React from 'react'
-import { List, ListItem, ListItemText } from '@material-ui/core'
+import React, { useState } from 'react'
+import { List, ListItem, ListItemText, ListItemAvatar, Button, Modal } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import db from './firebase.js'
 import './Todo.css'
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Todo(props) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+  const updateTodo = () => {
+    db.collection('todos').doc(props.todo.id).set({
+      todo: input
+    }, {merge: true});
+    setOpen(false);
+  }
+
   return (
+    <>
+    <Modal className='todo__modal'
+      open={open}
+      onClose={e => setOpen(false)}
+    >
+      <div className={classes.paper}>
+        <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)}/>
+        <Button onClick={updateTodo} variant='contained' color='default'>Update Todo</Button>
+      </div>
+    </Modal>
+
     <List className='todo__list'>
-      <ListItem>
-        <ListItemText primary={props.todo} />
-      </ListItem>
+      <ListItemAvatar>
+        <ListItemText primary={props.todo.todo} />
+      </ListItemAvatar>
+      <button onClick={e => setOpen(true)}>Edit</button>
+      <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
     </List>
+    </>
   )
 }
 
